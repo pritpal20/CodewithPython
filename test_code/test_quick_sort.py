0e333094a2f1a0d6791f14a63b962b467f8e284c
Pritpal Singh Chhabra <pritpal20@gmail.com>
2023-07-01 12:42:26 +0530

    added a test case for quick sort

--- test_code/test_quick_sort.py
+++ test_code/test_quick_sort.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 
-from common.helper_functions import create_array
+from common.helper_functions import create_array,create_array_unsigned
 from Sorting.sorting import quick_sort
 
 @pytest.mark.parametrize(
@@ -17,6 +17,24 @@ def test_quick_sort(n):
     arr1 = [i for i in range(-n, n)]
 
     arr2 = create_array(n)
+    N = len(arr2)
+    quick_sort(arr2,0,N - 1)
+
+    assert arr1 == arr2
+
+@pytest.mark.parametrize(
+    "n",
+    [
+        (10),
+        (50),
+        (100),
+        (1000),
+    ],
+)
+def test_quick_sort2(n):
+    arr1 = [i for i in range(0, n)]
+
+    arr2 = create_array_unsigned(n)
     N = len(arr2)
     quick_sort(arr2,0,N - 1)
 
